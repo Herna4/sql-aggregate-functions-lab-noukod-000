a1698d5e5463757075f62c3953370c6d4473c102
Herna Michel <labellehernamichel@yahoo.fr>
2019-05-15 14:20:40 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -2,16 +2,18 @@
 
 def highest_student_gpa
   "SELECT MAX(gpa) FROM students;"
-
 end
 
 def lowest_student_gpa
+  "SELECT MIN(gpa) FROM students;"
 end
 
 def average_student_gpa
+  "SELECT AVG(gpa) FROM students;"
 end
 
 def total_tardies_for_all_students
+  "SELECT SUM(gpa) FROM students;"
 end
 
 def average_gpa_for_9th_grade
